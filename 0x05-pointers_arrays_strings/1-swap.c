d46a6510f98d0818ef7de82d575dc2dc202b84b2
ITOBITOO <attarymarouane@gmail.com>
2023-06-27 17:59:57 -0700

    Done

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,5 +1,11 @@
 #include "main.h"
-void swap_int(int *a, int *b);
+/**
+*  reset_to_98 - takes the pointer and updates the value
+*  @n: int to check
+*  Owned By Bwave/Bright
+*  Return: 0 is success
+*/
+void swap_int(int *a, int *b)
 {
 	int = c;
 
