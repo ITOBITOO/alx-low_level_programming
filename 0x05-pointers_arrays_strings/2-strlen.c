3779c1185fa383c10fc1f9a82208ef37dd302bee
ITOBITOO <attarymarouane@gmail.com>
2023-06-27 19:15:12 -0700

    Done

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,10 +1,14 @@
 #include "main.h"
-#include <unistd.h>  
+#include <unistd.h>
+/*
+ * _strlen return the length of a string
+ * Owned By Bwave
+ * @s: char to check
+ * Description: this will return the length of a string
+ * Return: 0 is success
+ */   
 int _strlen(char *s)
 {
-	/*
-	 *increak hgfdddfj bghjhjhgfghfjfj
-	 */
 	int a = 0;
 
 	for (; *s++;)
